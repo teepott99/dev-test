c632cfa47100ad964291cba1828b1dd35214c107
Mariah Dunn <mariahdunn@Mariahs-MBP.hsd1.in.comcast.net>
2018-10-12 12:48:58 -0400

    style examples

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -19,3 +19,21 @@
 //     // console.log(getCourseInfo("806e1f48-94d9-e511-80d2-005056a12f06"))
 //   }
 
+const courseApi = axios.create({
+  baseURL: 'https://titan.indwes.edu/api/programs/?language=en'
+})
+
+function getCourseInfo(id) {
+courseApi.get(id)
+.then(response => {
+  console.log(response.data)
+})
+.catch(err => {
+  console.error(err)
+})
+}
+
+document.getElementById("courseButton").onclick = function(){
+getCourseInfo("846e1f48-94d9-e511-80d2-005056a12f06");
+console.log("Works??")
+}
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,44 @@ var express = require('express');
 var router = express.Router();
 // var cData = require('https://titan.indwes.edu/api/programs/?language=en')
 
-var Request = require("request");
+// var Request = require("request");
 
-Request.get("https://titan.indwes.edu/api/programs/?language=en", (error, response, body) => {
-    if(error) {
-        return console.log(error);
-    }
-    console.log("console log => " + error, response, bpdy)
-    response.render('/', {
-      body,
-    })
-    console.log(JSON.parse(body));
-});
+// Request.get("https://titan.indwes.edu/api/programs/?language=en", (error, response, body) => {
+//     if(error) {
+//         return console.log(error);
+//     }
+//     console.log("console log => " + error, response, bpdy)
+//     response.render('/', {
+//       body,
+//     })
+//     console.log(JSON.parse(body));
+// });
+
+// var requestJson = require("request-json");
+// var jsonSchema = require("json-schema");
+
+// var client = requestJson.createClient('https://titan.indwes.edu/api');
+
+// client.get('/programs/?language=en', function(err, res, body) {
+//   if (!err && response.statusCode == 200) {
+//           var locals = JSON.parse(body);
+//           res.render('<YOUR TEMPLATE>', locals);
+//           console.log(locals);
+//       }
+//   // return console.log(body.rows[0].title);
+// });
+
+// const axios = require('axios');
+
+// axios.get('https://titan.indwes.edu/api/programs/?language=en')
+//     .then(response => {
+//         console.log(response.data.status);
+//         // console.log(response.data);
+//         res.send(response.data.status);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
 
 
 
